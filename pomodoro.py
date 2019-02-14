1572167dd5670ae04ad42ee031678e592f9b22ac
keeganosler <oslerkeegan@gmail.com>
2019-02-14 15:22:43 -0700

    add comments to explain:

--- pomodoro.py
+++ pomodoro.py
@@ -10,6 +10,7 @@ import winsound
 root = tkinter.Tk()
 root.withdraw()
 
+#starts the clock when called, for however much time its told to run for
 def run_clock(t_run):
     for t in range (t_run, -1, -1):
         minutes = int(t/60)
@@ -17,12 +18,14 @@ def run_clock(t_run):
         print(str(minutes) + ":" + str(seconds))
         time.sleep(1)
 
+#decides what tasks are performed during the short breaks
 def pick_short_tasks(counter, listex):
     short_tasks = ["refill water", "message girlfriend", "use the bathroom", "shoulder stretches"]
     exercise_todo = listex[counter]
     short_task_todo = short_tasks[counter]
     messagebox.showinfo("Time for a short break!!", "\n During this time you should do the following things:\n 1. " + exercise_todo + "\n 2. " + short_task_todo)
 
+#sets up the list of exercises (one to be performed each short break) from user input
 def setup_exercises():
     exercises_todo = []
     for i in range(0,4):
@@ -30,6 +33,11 @@ def setup_exercises():
         exercises_todo.append(exercise)
     return exercises_todo    
 
+#sets up exercises and number of pomdoros
+#initializes counters for number of breaks, number of (short) tasks and number of pomodoro sessions performed
+#pulls up messages boxes for when pomodoros start and finish
+#if 4 short breaks have been performed, a long break is performed
+#once the specified number of pomodoros have been performed, it says goodnight
 def run_timer():
     total_pomodoros = setup_pomodoro()
     exercises_todo = setup_exercises()
@@ -49,9 +57,11 @@ def run_timer():
             messagebox.showinfo("Your break is over!", "\nTime to get back to work!")
             break_counter += 1
 
+#determines from user input how many pomodoros are to be performed given the number of hours the user wants to be working for
 def setup_pomodoro():
     day_length = int(simpledialog.askstring("Welcome!", "Good day sir! \nHow long would you like your workday to be?"))
     num_pomodoro_sessions = int((day_length*3600) / 10500)
     return num_pomodoro_sessions
 
+#runs the program
 run_timer()
