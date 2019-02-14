cce4eb5087e0548562215926c6912566adf8ba9e
keeganosler <oslerkeegan@gmail.com>
2019-02-14 13:58:02 -0700

    add tkinter messagebox

--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,12 @@
 import time
 
+import tkinter
+from tkinter import messagebox
+import winsound
+
+root = tkinter.Tk()
+root.withdraw()
+
 def run_time(t_run):
     for t in range (t_run, -1, -1):
         minutes = int(t/60)
@@ -12,23 +19,25 @@ def pick_short_tasks(counter):
     short_tasks = ["refill water", "message girlfriend", "use the bathroom", "shoulder stretches"]
     exercise_todo = exercises[counter]
     short_task_todo = short_tasks[counter]
-    print("Time for a short break!!\n During this time you should do the following things:\n 1. " + exercise_todo + "\n 2. " + short_task_todo)
+    messagebox.showinfo("Time for a short break!!", "\n During this time you should do the following things:\n 1. " + exercise_todo + "\n 2. " + short_task_todo)
 
 def run_timer():
     do_pomodoro = True
     break_counter = 0
     task_counter = 0
     while(do_pomodoro):
+        messagebox.showinfo("Pomodoro started!", "\n If you wish to start a 25-minute work period, please click the button below.")
         run_time(8)
         if(break_counter >= 3):
-            print("Time for a long break!!\n During this time, you should do the following things:\n 1. Eat something. 2. Take out the dog")
+            messagebox.showinfo("Time for a long break!!", "\n During this time, you should do the following things:\n 1. Eat something. \n2. Take out the dog")
             run_time(10)
             do_pomodoro = False
         else:
             pick_short_tasks(task_counter)
             task_counter += 1
             run_time(5)
-            print("Your break is over! Time to get back to work!")
+            messagebox.showinfo("Your break is over!", "\nTime to get back to work!")
             break_counter += 1
 
 print(run_timer())
+
